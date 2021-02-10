a288dd0e948e69e90bc79a5b48605723eb3b9f31
RobertoGallego <groberto@student.42.fr>
2021-02-10 13:42:59 +0100

    index app

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import path from "path";
 
 import videosRoutes from "app/routes/videosRoutes";
 
@@ -11,8 +12,19 @@ app.set("port", process.env.PORT || 9000);
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use(videosRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../../ui', 'build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../../ui/', 'build', 'index.html'));
+    });
+}
+
+// console.log("--->", path.join(__dirname, '../../ui/build', 'build', 'index.html'));
 
 export default app;
